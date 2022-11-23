24b1c42129c7edc6860d1d9fb32d111aa859e8bf
Miskatur <Miskaturrahman34826@gmail.com>
2022-11-24 02:41:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+//middlewear
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.lgwxtbx.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Furniclaim is running on Server')
+})
+
+app.listen(port, () => {
+    console.log(`Furniclaim is running on port: ${port}`);
+})
