ac8dc3c76a639fce057940e59a893dbaa9d5a6b8
Miskatur <Miskaturrahman34826@gmail.com>
2022-11-26 06:14:21 +0600

    products post done

--- index.js
+++ index.js
@@ -17,12 +17,32 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+        if (error) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const categoriesCollection = client.db('furniClaim').collection('categories');
         const productsCollection = client.db('furniClaim').collection('products');
         const usersCollection = client.db('furniClaim').collection('users');
 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN)
+            res.send({ token })
+        })
 
         app.get('/categories', async (req, res) => {
             const query = {};
@@ -37,6 +57,12 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).sort({ "_id": -1 }).toArray();
+            res.send(products)
+        })
+
         app.get('/products/:name', async (req, res) => {
             const name = req.params.name;
             const filter = { category: name }
@@ -44,12 +70,32 @@ async function run() {
             res.send(products)
         })
 
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product)
+            res.send(result)
+        })
+
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const result = await usersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email }
+            const result = await usersCollection.findOne(filter)
+            res.send(result)
+        })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
+
+
     }
     finally {
 
